9500516812302a444c62b380617dc5680a3dbd5b
Fernando <fvazquez@ucsd.edu>
2022-02-08 17:38:25 -0800

    Removed some unused imports

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { CSSTransition } from "react-transition-group";
 import Menu from "./Menu";
 import MobileMenu from "./MobileMenu";
 import { StyledJobsSection } from "./Experience.styled";
