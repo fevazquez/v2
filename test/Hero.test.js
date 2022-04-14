eacde2f7caec386b1431fab2ae9a63c98c285e18
Fernando <fvazquez@ucsd.edu>
2022-04-14 14:19:04 -0700

    Started to write up tests

--- test/Hero.test.js
+++ test/Hero.test.js
@@ -1,9 +1,24 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Hero from "../src/components/hero";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 
 // Test 1
 test("Renders Hero Text", () => {
-  render(<Hero />);
-  const nameElement = screen.getByText(/Fernando Vazquez/i);
-  expect(nameElement).toBeInTheDocument();
+  const Hello = () => (
+    <div>
+      Hello <span>world</span>
+    </div>
+  );
+  const { getByText } = render(<Hello />);
+
+  getByText((content, node) => {
+    const hasText = (node) => node.textContent === "Hello World";
+    const nodeHasText = hasText(node);
+    const childrenDontHaveText = Array.from(node.children).every(
+      (child) => !hasText(child)
+    );
+
+    return nodeHasText && childrenDontHaveText;
+  });
 });
