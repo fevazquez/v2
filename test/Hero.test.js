e9a1b4b944430c7d8bc49faf78d39a0319d3bf75
Fernando Vazquez <f4vazquez@gmail.com>
2022-09-22 23:17:16 -0700

    Hero unit tests are integerated

--- test/Hero.test.js
+++ test/Hero.test.js
@@ -1,7 +1,51 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 import Hero from "../src/components/hero";
-import { toBeInTheDocument } from "@testing-library/jest-dom";
+
+afterEach(cleanup);
 
 // Test 1
-test("Renders Hero Text", () => {});
+test("Renders Hero Text Greeting", () => {
+  jest.useFakeTimers(); // used for the useEffect hook to render the complete hero
+
+  render(<Hero />);
+  jest.advanceTimersByTime(1000);
+  expect(screen.getByText("Hello, I am")).toBeInTheDocument();
+});
+
+// Test 2
+test("Renders Hero Text Name", () => {
+  jest.useFakeTimers(); // used for the useEffect hook to render the complete hero
+
+  render(<Hero />);
+  jest.advanceTimersByTime(1000);
+  expect(screen.getByText("Fernando Vazquez.")).toBeInTheDocument();
+});
+
+// Text 3
+test("Renders Hero Text Title", () => {
+  jest.useFakeTimers(); // used for the useEffect hook to render the complete hero
+
+  render(<Hero />);
+  jest.advanceTimersByTime(1000);
+  expect(
+    screen.getByText("Software developer and DevOps enthusiast.")
+  ).toBeInTheDocument();
+});
+
+// Test 4
+test("Renders Hero Current Role", () => {
+  jest.useFakeTimers(); // used for the useEffect hook to render the complete hero
+
+  render(<Hero />);
+  jest.advanceTimersByTime(1000);
+  expect(screen.getByRole("link", "Amazon AWS.")).toBeInTheDocument();
+});
+
+// Test 5
+test("Renders Image", () => {
+  render(<Hero />);
+  expect(screen.getByAltText("yours truly")).toBeDefined();
+});
